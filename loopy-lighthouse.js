2b91891313f49bf79a144fd222ffcbffe96fffe7
alexhauka <alexhauka@gmail.com>
2020-11-17 22:17:13 +0000

    fizzbuzz exercise

---
+++ loopy-lighthouse.js
@@ -0,0 +1,11 @@
+for (let num = 100; num < 201; num++) {
+  if (num % 3 === 0 && num % 4 !== 0) {
+    console.log("Loopy");
+  } else if (num % 3 !== 0 && num % 4 === 0) {
+    console.log("Lighthouse");
+  } else if (num % 3 === 0 && num % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else {
+    console.log(num);
+  }
+}
